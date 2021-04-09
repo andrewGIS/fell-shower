c6a0b4236bf0dc4d6dabc7c7086bd306b9e08d0b
Mister A <andrew.tarasov1993@gmail.com>
2021-04-09 17:03:40 +0500

    add style and layer for predict and cloud

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,7 +3,16 @@
     <l-map :center="[59, 59]" :zoom="6">
       <l-tile-layer :url="url"></l-tile-layer>
       <!-- <l-geo-json :geojson="data" :options="optionsGeoJSON"></l-geo-json> -->
-      <l-geo-json v-if="selectedCoudMask" :geojson="selectedCoudMask" :options="optionsGeoJSON"></l-geo-json>
+      <l-geo-json
+        v-if="selectedCoudMask"
+        :geojson="selectedCoudMask"
+        :options-style="cloudStyle"
+      ></l-geo-json>
+      <l-geo-json
+        v-if="selectedPredict"
+        :geojson="selectedPredict"
+        :options="optionsPredictGeoJSON"
+      ></l-geo-json>
     </l-map>
   </v-container>
 </template>
@@ -13,7 +22,7 @@
 
 import { LMap, LTileLayer, LGeoJson } from "vue2-leaflet";
 import { testLayer } from "../assets/sampleGeojson";
-import { mapMutations} from "vuex";
+import { mapMutations } from "vuex";
 //console.log(JSON.parse(sampleJSON))
 //const  hydroPosts = hydroPosts
 export default {
@@ -23,7 +32,16 @@ export default {
     processing: false
   }),
   computed: {
-    optionsGeoJSON() {
+    cloudStyle() {
+      return () => {
+        return {
+          weight: 2,
+          color: "#F1F50F",
+          opacity: 1
+        };
+      };
+    },
+    optionsPredictGeoJSON() {
       return {
         onEachFeature: this.onEachFeature
       };
@@ -35,18 +53,19 @@ export default {
             e.target.feature.geometry.coordinates;
             this.SET_SELECTED_GEOM(e.target.feature.geometry);
             //console.log(wkt.stringify(e.target.feature.geometry));
-           
           }
         });
       };
     },
-    selectedCoudMask(){
-      return this.$store.state.cloudMaskGeoJSON
+    selectedCoudMask() {
+      return this.$store.state.cloudMaskGeoJSON;
+    },
+    selectedPredict() {
+      return this.$store.state.predictedGeoJSON;
     }
   },
   methods: {
-    ...mapMutations(["SET_SELECTED_GEOM"]),
-
+    ...mapMutations(["SET_SELECTED_GEOM"])
   },
   components: { LMap, LTileLayer, LGeoJson }
 };
